a71263da18ce116f876150e4e7a5924c86f5cd82
Ryan Bishop <rdbishop06@gmail.com>
2020-03-19 03:12:57 -0500

    wip: link tags opening in new tabs in browser now

--- src/components/feed/Article.js
+++ src/components/feed/Article.js
@@ -12,10 +12,10 @@ export default function Article({ article, number, methods }) {
                 <div className="arrow-up"></div>
             </td>
 			<td>
-			    <a href={article.link} title={article.link} className="article-link">
+			    <a href={article.link} rel="noopener noreferrer" target="_blank" title={article.link} className="article-link">
     				{article.title}
     			</a>
-    			<span className="external-link">(<a href={article.link} className="feedname" title={article.link}>{article.feed.name}</a>)</span>
+    			<span className="external-link">(<a rel="noopener noreferrer" href={article.link} target="_blank" className="feedname" title={article.link}>{article.feed.name}</a>)</span>
     			<p className="timestamp">{article.pub_date && moment(article.pub_date).fromNow()}</p>
 			</td>
             <td title="view tl;drs" onClick={methods.getSummaries} className="tldr-link">
