f352bb58ab93a180565a71e35b8df1add8c3e5da
Ryan Bishop <rdbishop06@gmail.com>
2020-03-30 18:17:28 -0500

    feat: can see if a summary is edited

--- src/components/feed/UserSummary.js
+++ src/components/feed/UserSummary.js
@@ -3,6 +3,10 @@ import moment from 'moment';
 import './UserSummary.css';
 
 export default function UserSummary({ userSummary, methods, isCurrentUser, listView, index }) {
+
+	// check if post has been edited if the updated_on field is greater than 5 minutes diff of created_on
+	const timeDiff = moment(userSummary.updated_on).diff(userSummary.created_on, 'minutes')
+
 	return (
 		<React.Fragment>
 			<tr>
@@ -16,7 +20,7 @@ export default function UserSummary({ userSummary, methods, isCurrentUser, listV
 				)}
 				<td>
 					<span className="username">{userSummary.user.username}</span>
-					<span className="timestamp">{moment(userSummary.created_on).fromNow()}</span>
+					<span className="timestamp">{moment(userSummary.created_on).fromNow()}{" "}{timeDiff > 5 && "(edited)"}</span>
 					<div className="user-summary">
 						<span className="tldr-header">TL;DR: </span>
 						<span>{userSummary.summary_text}</span>
