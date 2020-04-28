85b52965a08f604666fa34a1f9bf361c4e6103e4
Ryan Bishop <rdbishop06@gmail.com>
2020-04-28 01:54:24 -0500

    Cleanup

--- src/components/feed/SubscribeToFeedButton.js
+++ src/components/feed/SubscribeToFeedButton.js
@@ -5,9 +5,23 @@ import { Link } from 'react-router-dom'
 export default function SubscribeToFeedButton({ params, source, updateFeedSubscription }) {
     return (
         <span className="button-container subscribe">
-            {isAuthenticated() ? <button onClick={()=>updateFeedSubscription(params.feedId)}>
-                Add {source.name} to my feed
-            </button> : <Link style={{color:"orange"}} to={{pathname:"/login", state:{feedId: source.id }}}>Login to subscribe to feed sources</Link>}
+            {
+                isAuthenticated() 
+                ? 
+                <button onClick={()=>updateFeedSubscription(params.feedId)}>
+                    Add {source.name} to my feed
+                </button> 
+                : 
+                <Link 
+                    style={{color:"orange"}} 
+                    to={{
+                        pathname:"/login", 
+                        state: { feedId: source.id }
+                    }}
+                >
+                    Login to subscribe to feed sources
+                </Link>
+            }
         </span>
     )
 }
